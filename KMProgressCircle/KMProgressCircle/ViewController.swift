7ca22769a00dcc6ed40be9819864b58f6b1c0641
Kieran McGrady <kieran@hotrodsoftware.com>
2014-10-04 18:11:59 +0100

    Demo

--- KMProgressCircle/KMProgressCircle/ViewController.swift
+++ KMProgressCircle/KMProgressCircle/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var progressCircle :KMProgressCircle!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func progressSliderMoved(sender :AnyObject) {
+        var slider = sender as UISlider
+        progressCircle.progress = CGFloat(slider.value)
+        progressCircle.updateLayerProperties()
+    }
 }
 
