91b75f235332a1900bd8d2b4a1c0b9037b2a61e5
Kieran McGrady <kieran@hotrodsoftware.com>
2014-10-04 14:41:38 +0100

    First commit

---
+++ KMProgressCircle.swift
@@ -0,0 +1,22 @@
+//
+//  KMProgressCircle.swift
+//  KMProgressCircle
+//
+//  Created by Kieran McGrady on 04/10/2014.
+//  Copyright (c) 2014 HotRod Software. All rights reserved.
+//
+
+import UIKit
+
+class KMProgressCircle: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect)
+    {
+        // Drawing code
+    }
+    */
+
+}
